01d9b4354528d66289d8e61a83984e2d68b6a90c
Kavya Sree Kammari <110734776+Cosmos-Atom@users.noreply.github.com>
2025-04-26 13:31:58 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd /home/site/wwwroot
 
-# Download spaCy model
-echo "Downloading spaCy model..."
-python -m spacy download en_core_web_sm
-
 # Start the application
 echo "Starting application..."
 gunicorn app:app -b 0.0.0.0:8000 --timeout 1200 --workers 1 --preload
